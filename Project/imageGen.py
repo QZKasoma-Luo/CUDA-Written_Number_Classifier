5993eaa22be7e58525e540a497756742265e625e
alexgar1 <132021517+alexgar1@users.noreply.github.com>
2024-10-16 22:39:22 -0700

    Update imageGen.py

--- Project/imageGen.py
+++ Project/imageGen.py
@@ -1,34 +1,33 @@
 import numpy as np
 
+''' this script creates a sample image with a checkerboard pattern to test mechanics ''' 
+
 def save_as_idx_format(filename, image):
-    # Ensure the image is in uint8 format
     image = image.astype(np.uint8)
     
     # Define the IDX header
-    magic_number = 0x00000803  # Magic number for 3-dimensional unsigned byte array
-    num_images = 1             # We're saving just one image
-    rows = image.shape[0]      # 28
-    cols = image.shape[1]      # 28
+    magic_number = 0x00000803  #
+    num_images = 1           
+    rows = image.shape[0]     
+    cols = image.shape[1]      
     
     # Create the header as per IDX file format
     header = np.array([
-        0x00, 0x00, 0x08, 0x03,  # Magic number (for unsigned byte images)
-        0x00, 0x00, 0x00, num_images,  # Number of images (in this case, 1)
-        0x00, 0x00, 0x00, rows,   # Number of rows (28 for MNIST)
-        0x00, 0x00, 0x00, cols    # Number of columns (28 for MNIST)
+        0x00, 0x00, 0x08, 0x03,  # Magic number 
+        0x00, 0x00, 0x00, num_images,  # Number of images
+        0x00, 0x00, 0x00, rows,   # Number of rows 
+        0x00, 0x00, 0x00, cols    # Number of columns 
     ], dtype=np.uint8)
 
     # Open the file in binary write mode
     with open(filename, 'wb') as f:
-        # Write the header to the file
         f.write(header.tobytes())
-        # Write the image data to the file
         f.write(image.tobytes())
 
 # Set image size to 28x28
 size = 28
 
-# Create a 2D array for the alternating pattern (checkerboard)
+# Create a 2D array for the alternating pattern 
 checkerboard = np.fromfunction(lambda x, y: (x + y) % 2 * 255, (size, size))
 
 # Save the checkerboard image as an IDX file
